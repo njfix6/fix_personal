ec53becbfc4fcde75c8d8a750c344bba57d71811
unknown <fixxx044@umn.edu>
2015-06-22 19:45:59 -0700

    asfd

--- js/projects/project_controller.js
+++ js/projects/project_controller.js
@@ -1,7 +1,7 @@
 angular.module('project_controller', [])
 	.controller('project_controller', function($scope, Project_model) {
-		var winnie = new Project_model('Winnie The Pooh Wallpaper', 'winnie-the-pooh-wallpaper'),
-			topAlbums = new Project_model('My Top Albums', 'my-top-albums');
+		var winnie = new Project_model('Winnie The Pooh Wallpaper', 'winnie-the-pooh-wallpaper', 'projects'),
+			topAlbums = new Project_model('My Top Albums', 'my-top-albums', 'favorites');
 		$scope.projects = [winnie, topAlbums];
 		
 		
--- js/projects/project_model.js
+++ js/projects/project_model.js
@@ -1,8 +1,9 @@
 angular.module('Project_model',[])
   .factory('Project_model', function(){
-		var Project = function (name, viewFileName) {
+		var Project = function (name, viewFileName, projType) {
 			this.name = name;
-			this.viewFileName = viewFileName;	
+			this.viewFileName = viewFileName;
+			this.type = projType;	
 					
 		};
 		return Project;
