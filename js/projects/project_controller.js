74dc3b9f18ba50f033eac4666a6d394854ed85a9
Fix <nfix@SDGL13db4279a.local>
2015-06-27 08:25:50 -0700

    sad

--- js/projects/project_controller.js
+++ js/projects/project_controller.js
@@ -1,8 +1,9 @@
 angular.module('project_controller', [])
 	.controller('project_controller', function($scope, Project_model) {
 		var winnie = new Project_model('Winnie The Pooh Wallpaper', 'winnie-the-pooh-wallpaper', 'projects'),
-			topAlbums = new Project_model('My Top Albums', 'my-top-albums', 'favorites');
-		$scope.projects = [winnie, topAlbums];
+			topAlbums = new Project_model('My Top Albums', 'my-top-albums', 'favorites'),
+			resume = new Project_model('Resume', 'resume', 'work');
+		$scope.projects = [winnie, topAlbums, resume];
 		
 		
 	});
