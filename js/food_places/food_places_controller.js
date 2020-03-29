8397a851a257eaf85e9705b3fe6aa468b6816d13
Nicholas Fix <njfix6@gmail.com>
2020-03-28 22:59:01 -0700

    yep

--- js/food_places/food_places_controller.js
+++
@@ -1,4 +0,0 @@
-angular.module('food_places_controller', [])
-	.controller('food_places_controller', function($scope, Food_places_model) {
-		
-	}
--- js/food_places/food_places_model.js
+++
@@ -1,8 +0,0 @@
-angular.module('Food_places_model', [])
-	.controller('Food_places_model', function($scope) {
-		var FoodPlace = function(name) {
-			this.name = name;
-			this.location = "";
-		};
-		return FoodPlace;	
-	});
